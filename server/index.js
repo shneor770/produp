53318ffac4dc3d13e07d9beb37cf8d1799928799
shneor770 <57337950+shneor770@users.noreply.github.com>
2020-11-15 22:29:40 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,6 +10,9 @@ import postRoutes from "./routes/posts.js";
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
+});
 
 app.use(bodyParser.json({limit: '30mb', extended: true}))
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
